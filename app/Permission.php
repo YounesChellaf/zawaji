1070a27fdae362c35c39fb2d009c750cb97bce32
YounesChellaf <fy_chellaf@esi.dz>
2019-06-18 21:44:22 +0100

    auload problem checked

--- app/Permission.php
+++ app/Permission.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-
     public function roles(){
         return $this->belongsToMany('App\Role','permissions_roles');
     }
--- database/migrations/2014_10_12_000022_create_permissions_table.php
+++ database/migrations/2014_10_12_000022_create_permissions_table.php
@@ -15,7 +15,7 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->integer('name');
             $table->timestamps();
         });
     }
